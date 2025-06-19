d29f2a42cfffbfdb998263ee15cee9de1793543d
Peter McGaughey <peter.mcgaughey@gmail.com>
2025-06-18 23:24:03 -0400

    Add niri

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 nvidia-inst
-pacman -S nerd-fonts helix fish nushell xsel 
-yay -S walker google-chrome superfile-bin zen-browser-bin gitkraken
+sudo pacman -S nerd-fonts helix fish nushell xsel
+yay -S walker google-chrome superfile-bin zen-browser-bin gitkraken vesktop
 ln /usr/bin/helix /usr/bin/hx
 
 # Tiling Window Manager
 # Launcher
 yay -S walker ags-hyprpanel-git python-gpustat
-pacman -S dunst brightnessctl 
+sudo pacman -S dunst brightnessctl
 
 # Hyprland
-pacman -S hyprland dunst
+sudo pacman -S hyprland dunst
 yay -S walker
 
 # TUI
@@ -22,16 +22,18 @@ pacman -S superfile # you can call this with superfile or spf
 # pacman -S xplr # clean, only text, no preview
 
 # Terminal
-pacman -S kitty
+sudo pacman -S kitty
 # pacman -S ghostty # seems good, but mouse issues with GTK are a dealbreaker
 
 # Optional
 yay -S python-pywal16 # Run with 'wal' command
-pacman -S yt-dlp # Youtube downloader tool
-pacman -S system-config-printer
+sudo pacman -S yt-dlp # Youtube downloader tool
+sudo pacman -S system-config-printer
 
 # Programming
 yay -S cmake-language-server
-pacman -S lldb clang cmake
+sudo pacman -S lldb clang cmake
+
+sudo pacman -S niri fuzzel network-manager-applet
 
 xset r rate 250 40
